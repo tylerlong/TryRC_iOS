4cceb986e0ec64f8ba1d584250a7a685ca5d915b
Tyler Long <tyler4long@gmail.com>
2016-03-31 16:10:40 +0800

    Finish UI

--- TryRC_iOS/ViewController.cs
+++ TryRC_iOS/ViewController.cs
@@ -14,12 +14,32 @@ namespace TryRC_iOS
 		{
 			base.ViewDidLoad ();
 			// Perform any additional setup after loading the view, typically from a nib.
+
+			serverPickerView.Model = new ServerPickerViewModel ();
 		}
 
 		public override void DidReceiveMemoryWarning ()
 		{
 			base.DidReceiveMemoryWarning ();
 			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public class ServerPickerViewModel : UIPickerViewModel
+		{
+			public override nint GetComponentCount (UIPickerView pickerView)
+			{
+				return 1;
+			}
+
+			public override nint GetRowsInComponent (UIPickerView pickerView, nint component)
+			{
+				return 2;
+			}
+
+			public override string GetTitle (UIPickerView pickerView, nint row, nint component)
+			{
+				return (new string[2]{"Sandbox", "Production"})[row];
+			}
 		}
 	}
 }
--- TryRC_iOS/ViewController.designer.cs
+++ TryRC_iOS/ViewController.designer.cs
@@ -1,17 +1,77 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using Foundation;
+using System.CodeDom.Compiler;
 
 namespace TryRC_iOS
 {
 	[Register ("ViewController")]
 	partial class ViewController
 	{
+		[Outlet]
+		UIKit.UITextField appKeyTextField { get; set; }
+
+		[Outlet]
+		UIKit.UITextField appSecretTextField { get; set; }
+
+		[Outlet]
+		UIKit.UITextField messageTextField { get; set; }
+
+		[Outlet]
+		UIKit.UITextField passwordTextField { get; set; }
+
+		[Outlet]
+		UIKit.UITextField sendToTextField { get; set; }
+
+		[Outlet]
+		UIKit.UIPickerView serverPickerView { get; set; }
+
+		[Outlet]
+		UIKit.UITextField usernameTextField { get; set; }
+
+		[Action ("SendSMS:")]
+		partial void SendSMS (Foundation.NSObject sender);
+		
 		void ReleaseDesignerOutlets ()
 		{
+			if (appKeyTextField != null) {
+				appKeyTextField.Dispose ();
+				appKeyTextField = null;
+			}
+
+			if (appSecretTextField != null) {
+				appSecretTextField.Dispose ();
+				appSecretTextField = null;
+			}
+
+			if (serverPickerView != null) {
+				serverPickerView.Dispose ();
+				serverPickerView = null;
+			}
+
+			if (usernameTextField != null) {
+				usernameTextField.Dispose ();
+				usernameTextField = null;
+			}
+
+			if (passwordTextField != null) {
+				passwordTextField.Dispose ();
+				passwordTextField = null;
+			}
+
+			if (sendToTextField != null) {
+				sendToTextField.Dispose ();
+				sendToTextField = null;
+			}
+
+			if (messageTextField != null) {
+				messageTextField.Dispose ();
+				messageTextField = null;
+			}
 		}
 	}
 }
